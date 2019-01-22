a50ec6601c55ed8e7488cfd21fea147da6c180e0
shraddha1507 <sgupta.chk1507@gmail.com>
2019-01-22 10:50:17 +0530

    add

--- admin/application/views/admin/registrationform.php
+++ admin/application/views/admin/registrationform.php
@@ -33,13 +33,56 @@
                                        </tr>
 
 
-                                       <tr><td> <h4>Contact details : </h4></td><td><?php echo $row->contactdetail;?></td></tr>
+                                           <!-- <tr><h2><center>REGISTRATION FORM</center></h2></tr>
+                                            <tr>
+                                       <td> <h4>Student name :</h4></td> <td> <?php echo $row->sname; ?></td></tr>
+                                        <tr><td> <h4>Date of birth : </h4></td><td><?php echo $row->dob;?></td></tr>
+                                           <tr><td> <h4>Age : </h4></td><td><?php echo $row->age;?></td></tr>
+                                           <tr><td>  <h4>Addmission forclass : </h4></td><td><?php echo $row->addforclass;?></td></tr>
+                                           <tr><td><h4>Gender(male/female) : </h4></td><td><?php echo $row->gender;?></td></tr>
+                                            <tr><td> <h4>Nationality : </h4></td><td><?php echo $row->nation;?></td></tr>
+                                          <tr><h3><?php echo $row->father;?></h3></tr>
+                                             <tr><td><h4>Father'name : </h4></td><td><?php echo $row->fname;?></td></tr>
+                                               <tr><td><tr><td>  <h4>Occupation : </h4></td><td><?php echo $row->foccupation;?></td></tr>
+                                               <tr><td><h4>Education : </h4></td><td><?php echo $row->fedu;?></td></tr>
+                                                <tr><td><h4>Language spoken at home : </h4></td><td><?php echo $row->flanguage;?></td></tr>
+                                                 <tr><td> <h4>Residential number : </h4></td><td><?php echo $row->resiaddress;?></td></tr>
+                                                 <tr><td> <h4>Contact details : </h4></td><td><?php echo $row->contactdetail;?></td></tr>
                                                   <tr><td><h4>Phone no. : </h4></td><td><?php echo $row->phone;?></td></tr>
                                                   <tr><td><h4>Mobile no : </h4></td><td><?php echo $row->mobile;?></td></tr>
                                                    <tr><td><h4>Email : </h4></td><td><?php echo $row->email;?></td></tr>
-                                                  
-                                        </tbody>
-                                         
+                                                   <tr><h3><?php echo $row->mother;?></h3></tr>
+                                                   <tr><td><h4> Mother's name: </h4></td><td><?php echo $row->mname;?></td></tr>
+                                                    <tr><td><h4>Occupation : </h4></td><td><?php echo $row->moccupation;?></td></tr>
+                                                     <tr><td><h4>Education : </h4></td><td><?php echo $row->meducation;?></td></tr>
+                                                      <tr><td> <h4>Language spoken at home : </h4></td><td><?php echo $row->mlanguage;?></td></tr>-->
+                                                      <?php }?>
+
+
+                                       <!-- <tbody>
+                                        	<?php $i = 1;?>
+                                        	<?php $res = $this->db->get("regenquiry")->result();?>
+                                        	<?php foreach($res as $row):?>
+                                            <tr>
+                                              
+                                             
+                                                <td><?php echo $row->name; ?></td>
+                                                <td><?php echo $row->email; ?></td>
+                                                   <td><?php echo $row->contact; ?></td>
+                                                      <td><?php echo $row->ques; ?></td>
+                                                         <td><?php echo $row->enquiry_date; ?></td>
+                                                
+                                              
+                                                <td>
+                                                	<a href="<?php echo base_url();?>apanelForms/deleteCareer/<?php echo $row->id; ?>">
+                                                		Delete
+                                                	</a>
+                                                </td>
+                                            </tr>
+                                            <?php $i++; ?>
+                                            <?php endforeach;?>
+                                        </tbody>-->
+                                          <?php// endforeach;?>
                                        </table>  
                                     </div>
                                 </div>
