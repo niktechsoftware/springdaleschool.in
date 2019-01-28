16154135b32fb7822c94c86ccbb5b525a55779fc
shraddha1507 <sgupta.chk1507@gmail.com>
2019-01-28 14:20:50 +0530

    add

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -52,12 +52,14 @@ Spring Dale School adheres to quality Management System in line with ISO 9001 :2
 					<b><p style="color:green;" class="landing_col"><a href="<?php echo base_url();?>assets/img/sa1.pdf"> 2). Download Time Table for SA-1 Examination .</a><br><img src="<?php echo base_url(); ?>assets/img/new.png" height="30" width="50" alt="ad" /></p></b></a></p></b>
 					<?php $ft = $this->db->get("notice");
 					$i=1;	if($ft->num_rows()>0){
+?><table>
+
 							foreach($ft->result() as $ft):?>
-                        	<b><p style="color:green;" class="landing_col"><?php echo $i;?>.<?php echo $ft->message;?><br></p></b>
+	                        	<tr><td><b><p style="color:green;" class="landing_col"><?php echo $i;?>.<?php echo $ft->message;?><br></p></b></td></tr>
 							<?php $i++; endforeach;
 							}
 							?>
-					 
+					 </table>
                         </marquee>
 				
 					</div>
